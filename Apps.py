e0da84b94bc8e299db61e8937e560a152cdcf208
dessertcodeappetizer <aa6goal@outlook.com>
2024-12-15 23:21:27 +0530

    first commit

--- Apps.py
+++ Apps.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import requests.exceptions as err
 from datetime import datetime
 
 
@@ -10,23 +11,28 @@ def covidPage():
     data = {}
     if request.method == "POST":
         # response = requests.get("http://api.covid19api.com/summary", verify=False).json()
-        response = requests.get("https://data.covid19india.org/v4/min/data.min.json", verify=False).json()
-        user_in = request.form["user_in"]
-        
+        # response = requests.get("https://data.covid19india.org/v4/min/data.min.json", verify=False).json()
         try:
-            state_list = response[user_in]
-            information = state_list["total"]
-            data = {
-                "Confirmed": information["confirmed"], "Deceased": information["deceased"], "Recovered": information["recovered"], 
-                "Tested": information["tested"], "Vaccinated1": information["vaccinated1"], 
-                "Vaccinated2": information["vaccinated2"], "status": 200
-            }
-        except KeyError:
+            response = requests.get("https://api.rootnet.in/covid19-in/stats/latest", verify=False)
+        except (err.HTTPError, err.InvalidJSONError, err.InvalidProxyURL, err.ConnectionError, err.JSONDecodeError, err.RequestsWarning) as error:
+            print("Getting Exception as: ", error)
+        user_in = (request.form["user_in"]).lower()
+        if response.status_code == 200:
+            response = response.json()
+            information = response['data']['regional']
+            for i in range(len(information)):
+                if (information[i]['loc']).lower() == user_in:
+                    data = {"Confirmed": information[i]["confirmedCasesIndian"], 
+                            "Recovered": information[i]["discharged"], 
+                            "Deaths": information[i]["deaths"], 
+                            "Cases": information[i]["totalConfirmed"], 
+                            "status": 200}
+                    break
+        else:
             data = {"status": 404}
-        
-        
-    return render_template("covid.html", data = data)
-
+        if data == {}:
+            data = {"status": 405}
+    return render_template("covid.html", data=data)
 
 
 
@@ -39,7 +45,14 @@ def weatherPage():
         response = requests.get(url, verify=False).json()
         # print(response)
         if response["cod"] == 200:
-            data = {"City": response["name"], "Latitude": response["coord"]["lat"], "Longitude":response["coord"]["lon"], "Sunrise": datetime.fromtimestamp(response["sys"]["sunrise"]), "Sunset": datetime.fromtimestamp(response["sys"]["sunset"]), "Temperature": round(response["main"]["temp_max"]-273,2), "Humidity": response["main"]["humidity"], "status": 200}
+            data = {"City": response["name"], 
+                    "Weather": (response["weather"][0]["description"]).title(), 
+                    "Weather_code": f"{response['weather'][0]['id']}.jpg",
+                    "Temperature_now": round(response["main"]["temp"]-273, 2), 
+                    "Visibility": response["visibility"],
+                    "Humidity": response["main"]["humidity"], 
+                    "Wind_speed": response["wind"]["speed"],
+                    "status": 200}
         else:
             data = {"status": 404}
     return render_template("weather.html", data=data)
@@ -50,10 +63,3 @@ def homePage():
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
-
-
-
-
-
-
-
